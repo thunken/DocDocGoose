a3137591f4f4ebd3e845d48f700bcfa39aaab993
Damien Vannson <damien.vannson@gmail.com>
2019-01-30 18:35:39 +0100

    We now can add a request parameter in the dock block of the API methods

--- src/Tools/Generator.php
+++ src/Tools/Generator.php
@@ -2,7 +2,47 @@
 
 namespace Thunken\DocDocGoose\Tools;
 
+use Illuminate\Routing\Route;
+use Mpociot\Reflection\DocBlock\Tag;
+use ReflectionClass;
+
 class Generator extends \Mpociot\ApiDoc\Tools\Generator
 {
+
+    public function processRoute(Route $route, array $rulesToApply = [])
+    {
+        $parsedRoute = parent::processRoute($route, $rulesToApply);
+
+        $routeAction = $route->getAction();
+        list($class, $method) = explode('@', $routeAction['uses']);
+        $controller = new ReflectionClass($class);
+        $method = $controller->getMethod($method);
+
+        $routeGroup = $this->getRouteGroup($controller, $method);
+        $docBlock = $this->parseDocBlock($method);
+
+        $parsedRoute['request'] = $this->getRequestFromDocBlock($docBlock['tags']);
+
+        return $parsedRoute;
+    }
+
+    /**
+     * @param array $request
+     *
+     * @return bool
+     */
+    protected function getRequestFromDocBlock(array $tags)
+    {
+        $request = collect($tags)
+            ->first(function ($tag) {
+                return $tag instanceof Tag && strtolower($tag->getName()) === 'request';
+            });
+
+        if (!($request instanceof Tag)) {
+            return '';
+        }
+
+        return (string) $request->getContent();
+    }
 
 }
